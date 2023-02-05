797eb1e0cfd0c451a8f491bae81b2df759138248
Matheus Buchada <93539482+Buchadaz@users.noreply.github.com>
2023-02-05 17:40:29 -0300

    Add files via upload

---
+++ Operacoes.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBanco
+{
+    public class Operacoes
+    {
+        public Operacoes(int numeroConta, string nomeConta)
+        {
+            NumeroConta = numeroConta;
+            NomeConta = nomeConta;
+            Saldo = 0.0;
+        }
+
+        public Operacoes(int numeroConta, string nomeConta, double depositoInicial) : this(numeroConta, nomeConta)//Reaproveita o anterior
+        {
+            Deposito(depositoInicial); // Dessa forma acomapanha as mudanças da função
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5.00;
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " + NumeroConta 
+                + ", Titular: " 
+                + NomeConta + ", Saldo: $ " 
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        [Required]
+        [StringLength(6, ErrorMessage = "Esse campo precisa ser preenchido", MinimumLength = 3)]
+        public int NumeroConta { get; private set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Esse campo precisa ser preenchido", MinimumLength = 2)]
+        public string NomeConta { get; set;}
+
+        public double Saldo { get; private set; }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using ContaBanco;
+using System;
+using System.Globalization;
+
+namespace Course;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Operacoes operacoes;
+        Console.Write("Entre com o número da conta: ");
+        int numero = int.Parse(Console.ReadLine());
+        Console.Write("Entre com o nome do titular do conta: ");
+        string nome = Console.ReadLine();
+        Console.Write("Haverá depósito inicial(s/n) ? ");
+        char resp = char.Parse(Console.ReadLine());
+        if (resp == 's' || resp == 'S')
+        {
+            Console.Write("Digite o valor do deposito inicial: ");
+            double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            operacoes = new Operacoes(numero, nome, depositoInicial);
+
+        }
+        else { 
+            operacoes = new Operacoes(numero, nome); 
+        }
+        Console.WriteLine();
+        Console.WriteLine("Dados da conta:");
+        Console.WriteLine(operacoes);
+        Console.WriteLine();
+        Console.Write("Informe o valor de deposito: ");
+        double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        operacoes.Deposito(quantia);
+        Console.WriteLine("Dados da conta atualizados: ");
+        Console.WriteLine(operacoes);
+        Console.WriteLine();
+        Console.Write("Informe o valor de saque: ");
+        quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        operacoes.Saque(quantia);
+        Console.WriteLine("Dados da conta atualizados: ");
+        Console.WriteLine(operacoes);
+
+
+    }
+}
